194c490674019b991ed08f005b03cec9e6566ec9
HisokaCoding <zarniacci337@gmail.com>
2024-03-10 17:06:50 +0500

    Initial commit

---
+++ src/main/java/net/hisoka/hisokasfurniture/HisokasFurnitureClient.java
@@ -0,0 +1,10 @@
+package net.hisoka.hisokasfurniture;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HisokasFurnitureClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hisoka/hisokasfurniture/HisokasFurnitureDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hisoka.hisokasfurniture;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HisokasFurnitureDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
